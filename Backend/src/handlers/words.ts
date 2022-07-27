159f089183b77e5e6d5a366f5d98d6b46e76aad6
Ahmad Ramzy <ahmadramzy988@gmail.com>
2022-07-27 02:39:34 +0200

    feature: add random words endpoint

--- Backend/src/handlers/words.ts
+++ Backend/src/handlers/words.ts
@@ -15,8 +15,20 @@ const index = async (req: Request, res: Response) => {
   }
 };
 
+const getRandomWord = async (req: Request, res: Response) => {
+  try {
+    const word = await store.getRandom();
+    res.json(word);
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      res.status(500).json({ error: e.message });
+    }
+  }
+};
+
 const word_routes = (app: express.Application) => {
   app.get('/words-list', index);
+  app.get('/words-random', getRandomWord);
 };
 
 export default word_routes;
--- Backend/src/models/words.ts
+++ Backend/src/models/words.ts
@@ -11,4 +11,12 @@ export class WordStore {
     const result = await database.query(`SELECT * FROM words`);
     return result.rows;
   }
+
+  async getRandom(): Promise<Word[]> {
+    const result = await database.query(
+      `SELECT * FROM words WHERE pos = 'adjective' OR pos = 'adverb'
+         OR pos = 'noun' OR pos = 'verb' ORDER BY random() LIMIT 10`
+    );
+    return result.rows;
+  }
 }
